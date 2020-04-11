7868fe52b5e633c39aa46e90b51c5517cce5e716
mdiaz26 <mike.diaz006@gmail.com>
2020-04-11 17:27:22 -0400

    removed deletion of users from seeds

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -3,6 +3,27 @@ class Api::V1::ScoresController < ApplicationController
     # GET / scores
     def index
         scores = Score.all
-        render json: scores.to_json(only: [:id, :points, :distance, :user_id])
+        render json: scores.to_json(:include => {:user => {:only => :username}}, :except => [:created_at, :updated_at])
     end
+
+    def show
+        score = Score.find(params[:id])
+        render json: score.to_json(only: [:points, :distance, :user_id])
+    end
+
+    def create
+        score = Score.create(score_params)
+        if score.save
+            render json: score
+        else
+            render error: {error: 'Unable to create score'}, status: 400
+        end
+    end
+
+    private
+
+    def score_params
+        params.require(:score).permit(:points, :distance, :user_id)
+    end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,24 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: users.to_json(only: [:id, :username, :password])
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(only: [:id, :username, :password])
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render error: {error: 'Unable to create user'}, status: 400
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :id)
+    end
 end
